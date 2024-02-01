d5f024f2b1216f993e6e80435e13829a04da7105
Rajasreepv <118721676+Rajasreepv@users.noreply.github.com>
2024-02-01 19:06:40 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template,request
+from weather import getcurrentweather
+from waitress import serve
+
+app=Flask(__name__)
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+@app.route('/weather')
+def getweather():
+    city=request.args.get('city')
+    weatherdata=getcurrentweather(city)
+    if not weatherdata['cod'] == 200:
+        return render_template("not-found.html")
+    return render_template("weather.html",
+    title= weatherdata["name"],
+    status= weatherdata["weather"][0]["description"],
+    temp= f"{ weatherdata['main']['temp']:.1f}",
+  feels_like = f"{weatherdata['main']['feels_like']:.1f}"
+
+
+    )
+if __name__ == "__main__":
+    serve(app,host="0.0.0.0",port=8000)
---
+++ weather.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import requests
+import os
+load_dotenv()
+
+def getcurrentweather(city="kansas city"):
+    request_url=f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    weather_data=requests.get(request_url).json()
+    return weather_data
+
+if __name__ == "__main__":
+    print("Get Current Weather Conditions")
+    city=input("Enter city name")
+    getcurrentweather(city)
+    
