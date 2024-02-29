032039c74e2a76437e99d05fed12f63ba66f5078
Rajasree <118721676+Rajasreepv@users.noreply.github.com>
2024-02-29 22:06:42 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,11 +3,12 @@ from weather import getcurrentweather
 from waitress import serve
 
 app=Flask(__name__)
-@app.route('/')
-@app.route('/index')
 def fahrenheit_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5 / 9
     return round(celsius, 2)
+@app.route('/')
+@app.route('/index')
+
 def index():
     return render_template('index.html')
 @app.route('/weather')
@@ -17,12 +18,11 @@ def getweather():
     
     if not weatherdata['cod'] == 200:
         return render_template("not-found.html")
-    temp_max_celsius = fahrenheit_to_celsius(weatherdata['main']['temp_max'])
-    feels_like_celsius = fahrenheit_to_celsius(weatherdata['main']['feels_like'])
+
     return render_template("weather.html",
     title= weatherdata["name"],
     status= weatherdata["weather"][0]["description"],
-    temp= temp_max_celsius,
-  feels_like = feels_like_celsius)
+    temp= fahrenheit_to_celsius(weatherdata['main']['temp_max']),
+  feels_like = fahrenheit_to_celsius(weatherdata['main']['feels_like']))
 if __name__ == "__main__":
-    serve(app,host="0.0.0.0",port=3000)
+    serve(app,host="0.0.0.0",port=8000)
