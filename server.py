74edb2bbd10d1f05440b4fe92cfa9c6b6495f343
Rajasree <118721676+Rajasreepv@users.noreply.github.com>
2024-02-29 17:23:27 +0530

    Update server.py

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ def getweather():
                            
     title= weatherdata["name"],
     status= weatherdata["weather"][0]["description"],
-    temp = fahrenheit_to_celsius(weatherdata['main']['temp'])
+    temp = fahrenheit_to_celsius(weatherdata['main']['temp']),
     feels_like = fahrenheit_to_celsius(weatherdata['main']['feels_like'])
   #   temp= f"{ weatherdata['main']['temp']:.1f}",
   # feels_like = f"{weatherdata['main']['feels_like']:.1f}"
