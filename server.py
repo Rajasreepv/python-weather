0bfe8b3da97cac30c975319405e0a99bc10cc4cf
Rajasree <118721676+Rajasreepv@users.noreply.github.com>
2024-02-29 21:41:37 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,27 @@
 import math
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 from weather import getcurrentweather
 from waitress import serve
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 def kelvin_to_celsius(kelvin):
     celsius = kelvin - 273.15
     return round(celsius, 2)
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
 @app.route('/weather')
 def getweather():
-     city = request.args.get('city')
-     weatherdata = getcurrentweather(city)
+    city = request.args.get('city')
+    weatherdata = getcurrentweather(city)
     
     if 'data' not in weatherdata or 'values' not in weatherdata['data']:
         return render_template("not-found.html")
-
+    
     temperature_kelvin = weatherdata['data']['values']['temperature']
     
     
@@ -26,24 +29,6 @@ def getweather():
                            title=weatherdata["location"]["name"],
                            status=weatherdata["data"]["values"]["weatherCode"],
                            temp=temperature_kelvin)
-    # city=request.args.get('city')
-    # weatherdata=getcurrentweather(city)
-    # if not weatherdata['cod'] == 200:
-    #     return render_template("not-found.html")
-    
-    # return render_template("weather.html",
-                       
-    # title= weatherdata["name"],
-    # status= weatherdata["weather"][0]["description"],
-    # temp = fahrenheit_to_celsius(weatherdata['main']['temp_max']),
-    # feels_like = fahrenheit_to_celsius(weatherdata['main']['feels_like']))
-  #   temp= f"{ weatherdata['main']['temp']:.1f}",
-  # feels_like = f"{weatherdata['main']['feels_like']:.1f}"
-
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
-
-
-    
-
