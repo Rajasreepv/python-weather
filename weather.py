d6153f388db9f242619b22f64f8f5d8afa85b584
Rajasree <118721676+Rajasreepv@users.noreply.github.com>
2024-02-29 20:02:30 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -4,9 +4,20 @@ import os
 load_dotenv()
 
 def getcurrentweather(city="kansas city"):
-    request_url=f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
-    weather_data=requests.get(request_url).json()
-    return weather_data
+    
+    url = f"https://api.tomorrow.io/v4/weather/realtime?location={city}&apikey=jZDF6fRrJ9GhfBR50Np3sg3QS3amHG4g"
+
+    # Send a GET request to the API
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.ok:
+        # Parse the JSON response
+        weather_data = response.json()
+        return weather_data
+    # request_url=f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    # weather_data=requests.get(request_url).json()
+    # return weather_data
 
 if __name__ == "__main__":
     print("Get Current Weather Conditions")
