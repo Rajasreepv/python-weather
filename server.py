b2415c79107262cdb3ce40ef6d88e3a5cb089d98
Rajasree <118721676+Rajasreepv@users.noreply.github.com>
2024-02-29 21:51:56 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,34 +1,26 @@
-import math
-from flask import Flask, render_template, request
+from flask import Flask,render_template,request
 from weather import getcurrentweather
 from waitress import serve
 
-app = Flask(__name__)
-
-def kelvin_to_celsius(kelvin):
-    celsius = kelvin - 273.15
-    return round(celsius, 2)
-
+app=Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
-
 @app.route('/weather')
 def getweather():
-    city = request.args.get('city')
-    weatherdata = getcurrentweather(city)
+    city=request.args.get('city')
+    weatherdata=getcurrentweather(city)
     
-    if 'data' not in weatherdata or 'values' not in weatherdata['data']:
+    if not weatherdata['cod'] == 200:
         return render_template("not-found.html")
-    
-    temperature_kelvin = weatherdata['data']['values']['temperature']
-    
-    
     return render_template("weather.html",
-                           title=weatherdata["location"]["name"],
-                           status=weatherdata["data"]["values"]["weatherCode"],
-                           temp=temperature_kelvin)
+    title= weatherdata["name"],
+    status= weatherdata["weather"][0]["description"],
+    temp= f"{ weatherdata['main']['temp']:.1f}",
+  feels_like = f"{weatherdata['main']['feels_like']:.1f}"
 
+
+    )
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+    serve(app,host="0.0.0.0",port=3000)
