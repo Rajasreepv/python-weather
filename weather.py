bf333250c8d2ab36dd41e9815bf400ad223eabcd
Rajasree <118721676+Rajasreepv@users.noreply.github.com>
2024-02-29 17:59:45 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,9 +2,10 @@ from dotenv import load_dotenv
 import requests
 import os
 load_dotenv()
-
+# https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 def getcurrentweather(city="kansas city"):
-    request_url=f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    # request_url=f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    request_url=f'https://api.openweathermap.org/data/3.0/onecall?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
     weather_data=requests.get(request_url).json()
     return weather_data
 
